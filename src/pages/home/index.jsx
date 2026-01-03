9d90db3557764039f9220d3d968c58edc2f16355
mfathony115 <mfathony115@gmail.com>
2026-01-03 07:25:37 +0700

    new home

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,17 @@
+import Hero from "../../components/hero"
+import About from "../../components/about"
+import Stats from "../../components/stats";
+import Service from "../../components/services";
+import Contact from "../../components/contact"
+const Component = () => {
+    return (
+        <>
+            <Hero />
+            <About />
+            <Stats />
+            <Service />
+            <Contact />
+        </>
+    );
+}
+export default Component
