7afd2bcec82fed405f52e77f846de858ec5f3ab4
mfathony115 <mfathony115@gmail.com>
2026-01-03 07:27:17 +0700

    new type again

---
+++ src/components/hero/index.jsx
@@ -0,0 +1,11 @@
+import Hero from "../../common/hero/Hero"
+const Component = () => {
+  return (
+    <>
+        <section id="home" className="hero section dark-background">
+            <Hero />
+        </section>
+    </>
+    );
+}
+export default Component
